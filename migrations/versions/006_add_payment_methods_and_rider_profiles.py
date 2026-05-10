dba5108d759354da821520300d69e130cd35324c
Ali Ahmed Burney <you@example.com>
2026-05-11 02:41:13 +0500

    fix driverPortal Issues of ride pop up by removing the toggle button feature  of updating the status

--- migrations/versions/006_add_payment_methods_and_rider_profiles.py
+++ migrations/versions/006_add_payment_methods_and_rider_profiles.py
@@ -3,7 +3,7 @@ from typing import Sequence, Union
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "006_add_payment_methods_and_rider_profiles"
+revision: str = "006_payment_methods_profiles"
 down_revision: Union[str, None] = "d4e5c6f7a1b2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
